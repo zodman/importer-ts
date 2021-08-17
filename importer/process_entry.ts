a0bfb84864ac21ff960d5e74b9c7d3184cbf2c85
Andres Vargas <andres@python3.ninja>
2021-08-16 23:49:42 -0500

    finish it

--- importer/process_entry.ts
+++ importer/process_entry.ts
@@ -20,31 +20,39 @@ class ProcessDB {
       log.error("Error in create", e, data)
     }
   }
-  async getOrCreate(table: any, whereCondition: object, data: object){
+  async getOrCreate(table: any, whereCondition: object, data?: object){
     const objTable = await this.find(table, whereCondition)
     if (objTable === null ){
-      return await this.create(table, data)
+      if (data) { 
+        return await this.create(table, data)
+      } else {
+        return await this.create(table, whereCondition)
+      }
     } else {
       return objTable
     }
   }
 }
 
+interface IId{
+  id: number
+  name: string
+}
 
 export  class ProcessEntry extends  ProcessDB {
   private entry
   private prisma
   constructor(entry: any, prisma: PrismaClient){
     super()
-    this.prisma = prisma
     this.entry = entry
+    this.prisma = prisma
   }
   async processProvince() {
     const provinceName: string = this.entry.province
     const whereCondition = {
       name: provinceName
     }
-    const province =  await this.getOrCreate(this.prisma.province, whereCondition, whereCondition)
+    const province =  await this.getOrCreate(this.prisma.province, whereCondition)
     return province
   }
   async processCity(province: {id: number}) {
@@ -60,12 +68,34 @@ export  class ProcessEntry extends  ProcessDB {
     const where = {
       name: this.entry.occupations
     }
-    return await this.getOrCreate(this.prisma.noc, where, where)
+    return await this.getOrCreate(this.prisma.noc, where)
+  }
+  async processCompany(){
+    const where = {name: this.entry.employer}
+    return await this.getOrCreate(this.prisma.company, where)
+  }
+  async process(city: IId, noc: IId, company:IId){
+
+    let data = {
+      positions: parseInt(this.entry.positions_approved),
+      noc_id: noc.id,
+      company_id: company.id,
+      city_id: city.id,
+      year: parseInt(this.entry.year),
+      phase: this.entry.phase,
+      company_address: this.entry.address,
+      company_zipcode: this.entry.zipcode
+    }
+    let where = {identify:data}
+    return await this.getOrCreate(this.prisma.entry, where,data)
+
   }
   async execute() {
     const province = await this.processProvince()
     const city = await this.processCity(province)
     const noc = await this.processNoc()
+    const company = await this.processCompany()
+    const entry = await this.process(city, noc, company)
   }
 }
 
